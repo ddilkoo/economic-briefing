bdb005ea82fce0fefe24a6b1571c2011761790de
ddilkoo <ddilkoo@gmail.com>
2026-08-06 14:49:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import requests
 import feedparser
 
-RSS_URL = "https://news.google.com/rss/search?q=%EA%B2%BD%EC%A0%9C&hl=ko&gl=KR&ceid=KR%3Ako"
+
+RSS_URLS = [
+    ("🇰🇷 국내 경제", "https://news.google.com/rss/search?q=한국경제"),
+    ("🇺🇸 해외 경제", "https://news.google.com/rss/search?q=미국경제"),
+    ("💱 금융시장", "https://news.google.com/rss/search?q=환율+금리"),
+    ("🏠 부동산", "https://news.google.com/rss/search?q=부동산+정책"),
+]
 
 DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1534793981769027594/1JxOyILur5MJen6gw4PWgkG-kmTZo8lLrK4uDRIDmvd-_L3oqx7UrRwAqBE4QUL_7DmH"
 
 
-news = feedparser.parse(RSS_URL)
+message = "📊 오늘의 경제 브리핑\n\n"
 
-message = "📌 오늘 경제 뉴스\n\n"
 
-for item in news.entries[:5]:
-    message += f"• {item.title}\n{item.link}\n\n"
+for category, url in RSS_URLS:
+
+    news = feedparser.parse(url)
+
+    message += f"{category}\n"
+
+    for item in news.entries[:3]:
+        title = item.title
+        link = item.link
+
+        message += f"• {title}\n"
+        message += f"{link}\n\n"
+
+    message += "-------------------\n"
 
 
 requests.post(
     DISCORD_WEBHOOK,
     json={
-        "content": message
+        "content": message[:1900]
     }
 )
